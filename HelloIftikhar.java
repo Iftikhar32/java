f5ce7f997e8e8122e2b0c8f845603a067cd7dbfd
IFTIKHAR AHMAD <157147809+Iftikhar32@users.noreply.github.com>
2024-03-08 20:04:30 +0500

    Add files via upload
    First Commit

---
+++ HelloIftikhar.java
@@ -0,0 +1,6 @@
+public class HelloIftikhar {
+    public static void main(String[] args) {
+        String name = "Iftikhar";
+        System.out.println("Hello, " + name + "! Welcome to your first Java code.");
+    }
+}
